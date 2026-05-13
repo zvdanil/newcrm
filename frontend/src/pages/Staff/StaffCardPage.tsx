b715e5ec8380be53fe7cfdd4f22a8e6d802a9229
zvdanil <Zv.danil@gmail.com>
2026-05-14 00:05:12 +0300

    фикс 2 ретроспективный пересчёт ставок

--- frontend/src/pages/Staff/StaffCardPage.tsx
+++ frontend/src/pages/Staff/StaffCardPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react'
+import { useState, useMemo, useEffect } from 'react'
 import { useParams, useNavigate } from 'react-router-dom'
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
 import { staffApi, type StaffMember, type StaffRate, type SalaryTransaction, type RateType, type RateCategory, type ValueMode } from '../../api/staff.api'
